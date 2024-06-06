79fa485dc09ab9389f1259c828e8eb0efbab5049
brainka <brainkarau@gmail.com>
2024-06-06 21:51:15 +1000

    added event listeners to input fields and doing basic validation

--- main.js
+++ main.js
@@ -2,33 +2,31 @@
 // fix aria issues
 // redo image inside the input field
 
-// const inputElements = document.querySelectorAll('input');
 const currencyFieldElement = document.querySelector('#currency-field');
 const numberOfPeopleInputElement = document.querySelector(
 	'#number-of-people-input'
 );
+const tipButtons = document.querySelectorAll('.select-tip button');
+const tipInputElement = document.querySelector('#button-input');
+const resetButtonElement = document.querySelector('#reset-button')
 
 let currencyAmount = undefined;
 let numberOfPeople = undefined;
+let customTip = undefined;
 
-// inputElements.forEach((element) =>
-// 	element.addEventListener('keypress', handleInputChange)
-// );
-
-console.log(currencyAmount);
+function handleTipButtonClick(e) {
+	e.preventDefault();
+	console.log('button clicked');
+	console.log(e.target.value);
+}
 
 function handleCurrencyInputChange(e) {
-	console.log(e.target.value);
+	console.log(`currency ${e.target.value}`);
 	currencyAmount = e.target.value;
-
-	render();
-
-
-    
 }
 
 function handleNumberOfPeopleChange(e) {
-	console.log(e.target.value);
+	console.log(`number of people ${e.target.value}`);
 }
 
 function handleInputValidation() {
@@ -43,14 +41,52 @@ function handleInputValidation() {
 	}
 }
 
-currencyFieldElement.addEventListener('input', handleCurrencyInputChange);
-numberOfPeopleInputElement.addEventListener(
-	'input',
-	handleNumberOfPeopleChange
+function handleInput(e) {
+	// console.log(e.target.id);
+	// console.log(e.target.value);
+
+	switch (e.target.id) {
+		case 'currency-field':
+			console.log(`currency field selected - ${e.target.value}`);
+			currencyAmount = e.target.value;
+			break;
+		case 'button-input':
+			console.log(`button input ${e.target.value}`);
+			customTip = e.target.value;
+			break;
+		case 'number-of-people-input':
+			console.log(`number-of-people-input ${e.target.value}`);
+			numberOfPeople = e.target.value;
+			break;
+		default:
+			break;
+	}
+
+	validateInput();
+}
+// ====
+tipButtons.forEach((button) =>
+	button.addEventListener('click', handleTipButtonClick)
 );
 
-function render() {
-	console.log(`in render function ${currencyAmount}`);
+function validateInput() {
 
-    console.log(typeof currencyAmount);
+
+
+
+	if (currencyAmount && numberOfPeople && customTip) {
+		console.log('all has been filled out');
+		resetButtonElement.removeAttribute('disabled')
+	} else {
+		resetButtonElement.setAttribute('disabled', '');
+	}
 }
+
+currencyFieldElement.addEventListener('input', handleInput);
+
+numberOfPeopleInputElement.addEventListener('input', handleInput);
+
+tipInputElement.addEventListener('input', handleInput);
+
+//enable reset button when all input fields have been filled in
+// do actual validation later
