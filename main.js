c08072312059f71cb4fd12e2440215701e554584
brainka <brainkarau@gmail.com>
2024-06-08 18:02:33 +1000

    completed tip calculator app

--- main.js
+++ main.js
@@ -7,56 +7,66 @@ const numberOfPeopleInputElement = document.querySelector(
 	'#number-of-people-input'
 );
 const tipButtons = document.querySelectorAll('.select-tip button');
-const tipInputElement = document.querySelector('#button-input');
-const resetButtonElement = document.querySelector('#reset-button')
+const customTipInputElement = document.querySelector('#button-input');
+const resetButtonElement = document.querySelector('#reset-button');
+const errorMessageElement = document.querySelector('.error-message');
 
 let currencyAmount = undefined;
 let numberOfPeople = undefined;
 let customTip = undefined;
+let fixedTip = undefined;
+let selectedFixedTip = undefined;
+let previousSelection = undefined;
+let errorMessage = false;
 
 function handleTipButtonClick(e) {
 	e.preventDefault();
-	console.log('button clicked');
-	console.log(e.target.value);
+	customTipInputElement.value = '';
+	previousSelection = selectedFixedTip;
+	selectedFixedTip = e.target;
+
+	if (previousSelection !== selectedFixedTip) {
+		selectedFixedTip?.classList.add('selected');
+		previousSelection?.classList.remove('selected');
+	} else {
+		previousSelection?.classList.add('selected');
+	}
+
+	fixedTip = e.target.value;
+
+	validateInput();
 }
 
-function handleCurrencyInputChange(e) {
-	console.log(`currency ${e.target.value}`);
-	currencyAmount = e.target.value;
-}
+function handleInputValidation(e) {
+	const numberRegex = /^\d*(\.\d{0,2})?$/;
+	const potentialValue = e.target.value + e.key;
 
-function handleNumberOfPeopleChange(e) {
-	console.log(`number of people ${e.target.value}`);
-}
-
-function handleInputValidation() {
-	console.log('input validation');
-
-	const numberRegex = /^[0-9]/;
-	// const numberRegex = /^\$?\d+(,\d{3})*(\.\d{2})?$/;
-
-	if (!numberRegex.test(e.key)) {
+	if (!numberRegex.test(potentialValue)) {
 		console.log('yay');
 		e.preventDefault();
 	}
 }
 
-function handleInput(e) {
-	// console.log(e.target.id);
-	// console.log(e.target.value);
+function resetTotals(e) {
+	if (e.target.value.length === 0) {
+		document.querySelector('.tip-amount').textContent = '$0.00';
+		document.querySelector('.tip-total').textContent = '$0.00';
+	}
+}
 
+function handleInput(e) {
 	switch (e.target.id) {
 		case 'currency-field':
-			console.log(`currency field selected - ${e.target.value}`);
 			currencyAmount = e.target.value;
+			resetTotals(e);
 			break;
 		case 'button-input':
-			console.log(`button input ${e.target.value}`);
 			customTip = e.target.value;
+			resetTotals(e);
 			break;
 		case 'number-of-people-input':
-			console.log(`number-of-people-input ${e.target.value}`);
 			numberOfPeople = e.target.value;
+			resetTotals(e);
 			break;
 		default:
 			break;
@@ -64,29 +74,71 @@ function handleInput(e) {
 
 	validateInput();
 }
-// ====
+
 tipButtons.forEach((button) =>
 	button.addEventListener('click', handleTipButtonClick)
 );
 
 function validateInput() {
+	let localCurrencyAmount = parseFloat(currencyAmount);
+	let localNumberOfPeople = parseInt(numberOfPeople);
+	let localFixedTip = parseFloat(fixedTip);
+	let localCustomTip = parseFloat(customTip);
 
+	errorMessage = localNumberOfPeople === 0;
+	errorMessageElement.classList[errorMessage ? 'remove' : 'add']('hidden');
+	numberOfPeopleInputElement.classList[errorMessage ? 'add' : 'remove'](
+		'error'
+	);
 
+	if (errorMessage) {
+		document.querySelector('.tip-amount').textContent = '$0.00';
+		document.querySelector('.tip-total').textContent = '$0.00';
+	}
 
+	if (
+		currencyAmount &&
+		numberOfPeople &&
+		(customTip || fixedTip) &&
+		!errorMessage
+	) {
+		let tipAmount =
+			(localCurrencyAmount * (localFixedTip || localCustomTip)) / 100;
+		console.log(tipAmount);
 
-	if (currencyAmount && numberOfPeople && customTip) {
-		console.log('all has been filled out');
-		resetButtonElement.removeAttribute('disabled')
+		let tipTotal = localCurrencyAmount + tipAmount;
+		document.querySelector('.tip-amount').textContent = (
+			tipAmount / localNumberOfPeople
+		).toFixed(2);
+		document.querySelector('.tip-total').textContent = (
+			tipTotal / localNumberOfPeople
+		).toFixed(2);
+
+		resetButtonElement.disabled = false;
 	} else {
-		resetButtonElement.setAttribute('disabled', '');
+		resetButtonElement.disabled = true;
 	}
 }
 
+function handleClickCustomTip(e) {
+	customTip = '';
+	selectedFixedTip?.classList.remove('selected');
+	resetTotals(e);
+	fixedTip = '';
+
+	validateInput();
+}
+
+function calculateTip() {
+	console.log(calculateTip);
+}
+
 currencyFieldElement.addEventListener('input', handleInput);
-
 numberOfPeopleInputElement.addEventListener('input', handleInput);
+customTipInputElement.addEventListener('input', handleInput);
+currencyFieldElement.addEventListener('keypress', handleInputValidation);
+numberOfPeopleInputElement.addEventListener('keypress', handleInputValidation);
+customTipInputElement.addEventListener('keypress', handleInputValidation);
 
-tipInputElement.addEventListener('input', handleInput);
+customTipInputElement.addEventListener('click', handleClickCustomTip);
 
-//enable reset button when all input fields have been filled in
-// do actual validation later
